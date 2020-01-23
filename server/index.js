582a3b35aeaaf06eaf62c375f4bd5bcfeaa4fc3a
Samuel Deuter <SamuelDeuter@gmail.com>
2020-01-23 11:18:36 -0800

    Implement express hello world

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.end('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}...`);
+});
