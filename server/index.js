a6dfe3b84f7ac29701045edf6e9276ac7ee44b0f
Samuel Deuter <SamuelDeuter@gmail.com>
2020-01-24 14:31:15 -0800

    Implement static route

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.end('Hello World!');
-});
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}...`);
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}...`);
 });
