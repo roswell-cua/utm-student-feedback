524242e8c0a924bd5b89499968f21ece77a04797
Samuel Deuter <SamuelDeuter@gmail.com>
2020-01-27 13:01:45 -0800

    Add morgan to server for logging

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
+
+app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
