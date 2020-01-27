e8138fc971e1c7ad3fa1f4b1f31fdce7810810fa
Samuel Deuter <SamuelDeuter@gmail.com>
2020-01-27 00:02:29 -0800

    Enable sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
+    devtool: 'source-map',
     entry: path.resolve(__dirname, 'client', 'app.jsx'),
     output: {
         path: path.resolve(__dirname, 'public'),
