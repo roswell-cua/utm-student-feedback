338e4c4ca139a322802569d31b3cd233c806d289
Samuel Deuter <SamuelDeuter@gmail.com>
2020-01-26 23:32:38 -0800

    Change name back to app.jsx

--- client/app.jsx
+++ client/app.jsx
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('review'));
