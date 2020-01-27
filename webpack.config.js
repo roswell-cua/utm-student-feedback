d6051278821f81da1c18794f15a1a28674cf183d
Samuel Deuter <SamuelDeuter@gmail.com>
2020-01-26 23:31:45 -0800

    Fix webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,15 @@
 const path = require('path');
-const SRC_DIR = path.join(__dirname, '/client');
-const DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
-    entry: `${SRC_DIR}/app.jsx`,
+    mode: 'development',
+    entry: path.resolve(__dirname, 'client', 'app.jsx'),
     output: {
+        path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
-        path: DIST_DIR
     },
     module: {
         rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            }
+            { test: /\.jsx?$/, loader: 'babel-loader', options: { presets: ['@babel/preset-react'] } }
         ]
     }
 };
