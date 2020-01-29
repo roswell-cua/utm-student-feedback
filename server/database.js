b7b6408f126494c6792419c08954c31e352f91bc
Samuel Deuter <SamuelDeuter@gmail.com>
2020-01-28 18:03:56 -0800

    Write schema, export database connection, schema, and model for testing

--- server/database.js
+++ server/database.js
@@ -1,2 +1,26 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/reviews');
+mongoose.connect('mongodb://localhost/reviews', { useNewUrlParser: true });
+
+const ReviewSchema = mongoose.Schema({
+  content: String,
+  rating: Number,
+  created: Date,
+  modified: Date,
+  user: String,
+  avatar: String,
+  response: {
+    content: String,
+    created: Date,
+    modified: Date,
+    user: String,
+    avatar: String
+  }
+});
+
+const Review = mongoose.model('Review', ReviewSchema);
+
+module.exports = {
+  connection: mongoose,
+  ReviewSchema,
+  Review
+};
