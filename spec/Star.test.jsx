f59800678b4b028d6f364397a2ad22c980cfa332
Samuel Deuter <SamuelDeuter@gmail.com>
2020-01-30 13:41:45 -0800

    Properly test if fill values exist

--- spec/Star.test.jsx
+++ spec/Star.test.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable no-undef */
+/* eslint-env jest */
 import React from 'react';
 import { shallow } from 'enzyme';
 import Star from '../client/components/Star';
@@ -13,12 +13,12 @@ describe('Star Component', () => {
   it('it should fill a default color', () => {
     const wrapper = shallow(<Star />);
 
-    expect(wrapper.find('[fill="#dedfe0"]')).toBe(true);
+    expect(wrapper.exists('[fill="#dedfe0"]')).toBe(true);
   });
 
   it('it should fill a highlighted color', () => {
     const wrapper = shallow(<Star highlighted />);
 
-    expect(wrapper.find('[fill="#f4c150"]')).toBe(true);
+    expect(wrapper.exists('[fill="#f4c150"]')).toBe(true);
   });
 });
