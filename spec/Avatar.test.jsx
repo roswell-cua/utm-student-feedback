df2acb556f54d193bdcc04d6fab83146583c9354
Samuel Deuter <SamuelDeuter@gmail.com>
2020-01-30 14:18:56 -0800

    Avatar test spec

---
+++ spec/Avatar.test.jsx
@@ -0,0 +1,24 @@
+/* eslint-env jest */
+import React from 'react';
+import { shallow } from 'enzyme';
+import Avatar from '../client/components/Avatar';
+
+describe('Avatar Component', () => {
+  it('should render an image when given an avatar', () => {
+    const wrapper = shallow(<Avatar avatar="localhost:3000/smiley.png" />);
+
+    expect(wrapper.containsMatchingElement(<img src="localhost:3000/smiley.png" alt="avatar" />)).toBe(true);
+  });
+
+  it('should render initials when given a name', () => {
+    const wrapper = shallow(<Avatar username="Sam Deuter" />);
+
+    expect(wrapper.text()).toBe('SD');
+  });
+
+  it('should not render initials when given both a name and avatar', () => {
+    const wrapper = shallow(<Avatar avatar="localhost:3000/smiley.png" username="Sam Deuter" />);
+
+    expect(wrapper.text()).not.toBe('SD');
+  });
+});
