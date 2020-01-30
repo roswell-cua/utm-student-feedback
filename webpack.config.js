e7df05f722faf96b45f9bbf487819137e5ddb585
Samuel Deuter <SamuelDeuter@gmail.com>
2020-01-29 16:17:30 -0800

    resolve '', '.js', '.jsx', and '.json' files in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,8 @@ module.exports = {
         rules: [
             { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', options: { presets: ['@babel/preset-env', '@babel/preset-react'] } }
         ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.json']
     }
 };
