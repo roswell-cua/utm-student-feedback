7bcf4e3e60f66d1c1c05e2714e3534ed5e24468e
Samuel Deuter <SamuelDeuter@gmail.com>
2020-01-30 14:19:43 -0800

    Switch node property to getElement method

--- spec/SearchBar.test.jsx
+++ spec/SearchBar.test.jsx
@@ -17,7 +17,7 @@ describe('SearchBar Component', () => {
     const mockSearch = jest.fn();
     const wrapper = shallow(<SearchBar onSearch={mockSearch} />);
 
-    wrapper.find('input').node.value = 'search string';
+    wrapper.find('input').getElement().value = 'search string';
     wrapper.find('[type="submit"]').simulate('click');
 
     expect(mockSearch.mock.calls[0][0]).toBe('search string');
