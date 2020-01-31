9cd79105f838825949d93b2cf3300d091a775686
Samuel Deuter <SamuelDeuter@gmail.com>
2020-01-30 16:41:13 -0800

    Refactor StarRating.test.jsx to be more dry

--- spec/StarRating.test.jsx
+++ spec/StarRating.test.jsx
@@ -4,15 +4,13 @@ import { shallow } from 'enzyme';
 import StarRating from '../client/components/StarRating';
 
 describe('StarRating Component', () => {
-  it('should render 5 star components', () => {
-    const wrapper = shallow(<StarRating rating={5} />);
+  const wrapper = shallow(<StarRating rating={3} />);
 
+  it('should render 5 star components', () => {
     expect(wrapper.find('Star')).toHaveLength(5);
   });
 
   it('should only render lit stars equal to the rating', () => {
-    const wrapper = shallow(<StarRating rating={3} />);
-
     expect(wrapper.find({ highlighted: true })).toHaveLength(3);
   });
 });
