681ea3efe0cd6117ace95197c8bbced709975a68
Samuel Deuter <SamuelDeuter@gmail.com>
2020-01-31 19:47:00 -0800

    Partially implement Review Component
    todo: refactor to use CSS grid to properly
    place data in the correct positions

--- client/components/Feedback.jsx
+++ client/components/Feedback.jsx
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 import PercentageBar from './PercentageBar';
 import StarRating from './StarRating';
 import RatingSelector from './RatingSelector';
+import Avatar from './Avatar';
+import Review from './Review';
 
 const Container = styled.div`
   font-family: open sans, helvetica neue, Helvetica, Arial, sans-serif;
@@ -16,12 +18,40 @@ const Heading = styled.div`
   font-size: 22px;
 `;
 
+const NOW = new Date().toString();
+
+const review = {
+  content: `I've tried many angular courses on Udemy so far. This is no doubt the best of them, and almost the best at all. It covers every tiny detail of the framework.Great explanation.....! hats up to the trainer nicely composed. From my side few suggestions, mentioning here 1. create a angular library and use it in another angular app 2. artifact our created library to npm store and add dependency to our another angular app. so that it can be install when we ran 'npm install'
+  was this helpful?`,
+  rating: 4,
+  created: NOW,
+  modified: NOW,
+  user: 'Sam Deuter',
+  avatar: 'https://i.udemycdn.com/user/50x50/60471148_2f11.jpg',
+};
+
+const reviewWithResponse = {
+  ...review,
+  response: {
+    content: `This is a response, NOW THIS IS A RESPONSE LET'S TYPE SOME MORE WORDS
+    YAYAYAYAYAYAYAYAYAY HAHAHA YO WADDUP G`,
+    created: NOW,
+    modified: NOW,
+    user: 'asdf111',
+    avatar: 'localhost:3000/smiley.png',
+  },
+};
+
 const Feedback = () => (
   <Container>
     <Heading>Student feedback</Heading>
     <PercentageBar percent={50} />
     <StarRating rating={3} />
     <RatingSelector percent={68} rating={3} />
+    <Avatar username="Sam Deuter" />
+    <Avatar username="Sam Deuter" src="https://i.udemycdn.com/user/50x50/60471148_2f11.jpg" />
+    <Review review={review} />
+    <Review review={reviewWithResponse} />
   </Container>
 );
 
--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -1,7 +1,108 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const Review = () => null;
+import Avatar from './Avatar';
+import StarRating from './StarRating';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  @media (max-width: 575px) {
+    flex-direction: column;
+  }
+`;
+
+const Profile = styled.div`
+  order: 1;
+  @media (max-width: 575px) {
+    display: none;
+  }
+`;
+
+const Info = styled.div`
+  order: 2;
+  @media (max-width: 575px) {
+    order: 2;
+  }
+`;
+
+const Content = styled.div`
+  order: 3;
+  @media (max-width: 575px) {
+    order: 1;
+  }
+`;
+
+const ResponseWrapper = styled.div`
+  padding: 10px;
+  background-color: #f7f8fa;
+  order: 4;
+  @media (max-width: 575px) {
+    order: 3;
+  }
+`;
+
+const Response = ({ response }) => {
+  if (response) {
+    return (
+      <ResponseWrapper>
+        <div>{`${response.user} (instructor), ${response.created}`}</div>
+        <div>{response.content}</div>
+      </ResponseWrapper>
+    );
+  }
+  // if there is no response object we don't want to
+  // render a component
+  return null;
+};
+
+Response.propTypes = {
+  response: PropTypes.shape({
+    user: PropTypes.string.isRequired,
+    created: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+  }),
+};
+
+Response.defaultProps = {
+  response: null,
+};
+
+const Review = ({ review }) => (
+  <Container>
+    <Profile>
+      <Avatar src={review.avatar} username={review.user} />
+    </Profile>
+    <Info>
+      <div>{review.user}</div>
+      <div>{review.created}</div>
+    </Info>
+    <Content>
+      <StarRating rating={review.rating} />
+      <div>{review.content}</div>
+      <Response response={review.response} />
+    </Content>
+  </Container>
+);
+
+
+Review.propTypes = {
+  review: PropTypes.shape({
+    content: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    created: PropTypes.string.isRequired,
+    modified: PropTypes.string.isRequired,
+    user: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    response: PropTypes.shape({
+      content: PropTypes.string,
+      created: PropTypes.string,
+      modified: PropTypes.string,
+      user: PropTypes.string,
+      avatar: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Review;
